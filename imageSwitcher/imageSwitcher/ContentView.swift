3a0ad609280877bd9b2fcfe0266975d4cf0826f0
chynmn <chynmn0523@gmail.com>
2023-01-30 17:18:24 +0900

    [feat] image switch app using button

--- imageSwitcher/imageSwitcher/ContentView.swift
+++ imageSwitcher/imageSwitcher/ContentView.swift
@@ -7,13 +7,62 @@
 
 import SwiftUI
 
+//struct ImageModifier: ViewModifier {
+//    func body(content: Content) -> some View {
+//        (content as! Image)
+//            .resizable()    //  이미지가 짤리지 않도록 함
+//            .frame(width: 250, height: 200)
+//    }
+//}
+
 struct ContentView: View {
+    static let names = [
+        "kym1",
+        "kym2",
+        "kym3",
+        "kym4",
+        "kym5",
+    ]
+    @State var page = 1
+    
+    let total = ContentView.names.count
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Button {
+                    if page > 1 {
+                        page -= 1
+                    }
+                } label: {
+                    Image(systemName: "arrow.left.circle")
+                        .resizable()
+                        .frame(width: 64, height: 64)
+                }
+                .disabled(page == 1)
+                Spacer()
+                Text("\(page) / \(total)")
+                    .font(.largeTitle)
+                Spacer()
+                Button{
+                    if page < ContentView.names.count {
+                        page += 1
+                    }
+                    
+                } label: {
+                    Image(systemName: "arrow.right.circle")
+                        .resizable()
+                        .frame(width: 64, height: 64)
+                }
+                .disabled(page == total)
+            }
+            Spacer()
+            Text("Hello, Kyoungmin!")
+                .font(.largeTitle)
+            Spacer()
+            Image(ContentView.names[page - 1])
+                .resizable()
+                .frame(width: 400, height: 300)
+            Spacer()
         }
         .padding()
     }
