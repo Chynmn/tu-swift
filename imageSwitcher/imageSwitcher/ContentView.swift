5565bc4d0eeaa9e484444dc2a1c23424a972c48e
chynmn <chynmn0523@gmail.com>
2023-01-30 16:19:52 +0900

    [feat] Create imageSwitcher

---
+++ imageSwitcher/imageSwitcher/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  imageSwitcher
+//
+//  Created by /Chynmn/M1 pro—̳͟͞͞♡ on 2023/01/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ imageSwitcher/imageSwitcher/imageSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageSwitcherApp.swift
+//  imageSwitcher
+//
+//  Created by /Chynmn/M1 pro—̳͟͞͞♡ on 2023/01/30.
+//
+
+import SwiftUI
+
+@main
+struct imageSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
