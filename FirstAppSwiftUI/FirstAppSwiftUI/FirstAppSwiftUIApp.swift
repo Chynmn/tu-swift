b0740baea96536e70c27a7539c9138d993400a88
chynmn <chynmn0523@gmail.com>
2023-01-30 12:20:09 +0900

    [feat] FirstApp using SwiftUI

---
+++ FirstAppSwiftUI/FirstAppSwiftUI/FirstAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppSwiftUIApp.swift
+//  FirstAppSwiftUI
+//
+//  Created by /Chynmn/M1 pro—̳͟͞͞♡ on 2023/01/30.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
