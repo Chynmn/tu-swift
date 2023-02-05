d051aa7b2108ea286045c7def5249bd435bb6f5a
chynmn <chynmn0523@gmail.com>
2023-02-05 10:54:44 +0900

    [feat] Tour1

---
+++ Tour_01.playground/Contents.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+var name:String = "GyonG"
+name = "12"
+var age = 20
+
+var bornIn = 1998
+
+age += 1
+bornIn += 1
+
+var seconds = 3.3
+seconds = 4 // Double 형 변수에 Int 값을 넣는 것은 괜찮다
+
+var value1 = 10.0
+var value2: Double = 10
+
+value1
+value2
+
+let pi = 3.14
+let msg = "value of π is \(pi)"
+print(msg)
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
